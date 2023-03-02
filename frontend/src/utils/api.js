cf224fbc362444a9e4677fb60acda05b94a84543
eduardoddmg <eduardoddmg@gmail.com>
2023-03-02 06:25:51 -0300

    update api base url on axios

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3001/api';
+const BASE_URL = 'https://contatos-mern.onrender.com/api';
 
 export const api = axios.create({
 	baseURL: BASE_URL
